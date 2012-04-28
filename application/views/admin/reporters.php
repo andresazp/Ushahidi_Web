702410da79088d917df00219b459409cfb8a20ce
Yauhen Kharuzhy <jekhor@gmail.com>
2012-04-28 08:25:50 +0300

    Remove dropped property reporter.service_userid from reporters view
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>
    
    Conflicts:
    
    	application/views/admin/reporters.php

--- application/views/admin/reporters.php
+++ application/views/admin/reporters.php
@@ -177,7 +177,6 @@
 										$level_id = $reporter->level_id;
 								        $service = new Service_Model($service_id);
 								        $service_name = $service->service_name;
-							    		$service_userid = $reporter->service_userid;
 							    		$service_account = $reporter->service_account;
 											if ($keyword)
 											{
